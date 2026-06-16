390c72b0ff44fbf686b51fd4cd76f7edfbe6bd10
Anthony Briceño <anthonyquiroz305@gmail.com>
2026-06-15 19:20:31 -0500

    Operaciones de flujo y de estado recien siempre el id de la sala

--- lib/trivia_crack_quiz.ex
+++ lib/trivia_crack_quiz.ex
@@ -1,14 +1,18 @@
 defmodule TriviaCrackQuiz do
   @moduledoc """
   API publica del juego Trivia Crack Quiz Multiplayer.
+
+  Todas las operaciones reciben el `room_id` de la sala sobre la que actuan,
+  ya que pueden coexistir varias partidas a la vez.
   """
 
-  def state, do: TriviaCrackQuiz.GameServer.state()
+  def state(room_id), do: TriviaCrackQuiz.GameServer.state(room_id)
 
-  def join(player_id, name), do: TriviaCrackQuiz.GameServer.join(player_id, name)
+  def join(room_id, player_id, name),
+    do: TriviaCrackQuiz.GameServer.join(room_id, player_id, name)
 
-  def start_game, do: TriviaCrackQuiz.GameServer.start_game()
+  def start_game(room_id), do: TriviaCrackQuiz.GameServer.start_game(room_id)
 
-  def answer(player_id, submitted_answer),
-    do: TriviaCrackQuiz.GameServer.answer(player_id, submitted_answer)
+  def answer(room_id, player_id, submitted_answer),
+    do: TriviaCrackQuiz.GameServer.answer(room_id, player_id, submitted_answer)
 end
